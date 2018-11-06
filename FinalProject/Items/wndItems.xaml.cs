7ff9268ea4280ba3dd58222356dee274a1851155
nathan <nathanlyon1@mail.weber.edu>
2018-11-05 18:53:35 -0700

    Added gui elements to the wndItems window
    Added gui elements to the wndItems window

--- FinalProject/Items/wndItems.xaml.cs
+++ FinalProject/Items/wndItems.xaml.cs
@@ -19,9 +19,25 @@ namespace FinalProject.Items
     /// </summary>
     public partial class wndItems : Window
     {
+        string[] arr;
         public wndItems()
         {
             InitializeComponent();
+            arr = new string[10] {"1","2","3","4","5","6","7","8","9","10" };
+            ListBox1.Items.Add(arr[0]);
+            ListBox1.Items.Add(arr[1]);
+            ListBox1.Items.Add(arr[2]);
+            ListBox1.Items.Add(arr[3]);
+            ListBox1.Items.Add(arr[4]);
+            ListBox1.Items.Add(arr[5]);
+            ListBox1.Items.Add(arr[6]);
         }
+
+        private void ListBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {            
+            Console.WriteLine(ListBox1.SelectedItem.ToString());
+        }
+
+        
     }
 }
