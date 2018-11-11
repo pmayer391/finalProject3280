380a832faca227b504435fd1ae3041a6f9066019
nathan <nathanlyon1@mail.weber.edu>
2018-11-10 20:02:18 -0700

    Added SQL statements to the clsItemsSQL class

--- FinalProject/Items/clsItemsSQL.cs
+++ FinalProject/Items/clsItemsSQL.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace FinalProject.Items
 {
-    class clsItemsSQL
+    public class clsItemsSQL
     {
+
+        public static string ItemInsertDescSQL(string ItemCode, string ItemDesc, double Cost)
+        {
+            return "INSERT INTO ItemDesc (ItemCode, ItemDesc, Cost) Values ('" 
+                + ItemCode +"', '" + ItemDesc + "', '" + Cost + "')";
+        }
+
+        public static string ItemUpdateDescSQL(string ItemCode, string ItemDesc, double Cost)
+        {
+            return "UPDATE ItemDesc SET ItemCode='" 
+                + ItemCode + "', ItemDesc = ' " + ItemDesc + " ', Cost = '" + Cost  
+                + "' WHERE ItemCode='" + ItemCode + "'";        
+        }
+
+        public static string ItemDeleteDescSQL(string ItemCode)
+        {
+            return "DELETE FROM ItemDesc WHERE ItemCode = '" +  ItemCode +"'";
+        }
+
+        public static string ItemSelectDescSQL(string ItemCode)
+        {
+            return "SELECT * FROM ItemDesc WHERE ItemCode = '" +  ItemCode +"'";
+        }
+
+        public static string ItemsSelectDescSQL()
+        {
+            return "SELECT * FROM ItemDesc";
+        }
+
+
     }
 }
