80c25f8146f0f566d53565939cda34d6794e4c6c
nathan <nathanlyon1@mail.weber.edu>
2018-11-16 21:37:25 -0700

    Made GUI improvments and added unimplemented method.

--- FinalProject/Items/clsItemsSQL.cs
+++ FinalProject/Items/clsItemsSQL.cs
@@ -9,12 +9,26 @@ namespace FinalProject.Items
     public class clsItemsSQL
     {
 
+        /// <summary>
+        /// Inserts a new Item into the database
+        /// </summary>
+        /// <param name="ItemCode"></param>
+        /// <param name="ItemDesc"></param>
+        /// <param name="Cost"></param>
+        /// <returns></returns>
         public static string ItemInsertDescSQL(string ItemCode, string ItemDesc, double Cost)
         {
-            return "INSERT INTO ItemDesc (ItemCode, ItemDesc, Cost) Values ('" 
+             return "INSERT INTO ItemDesc (ItemCode, ItemDesc, Cost) Values ('" 
                 + ItemCode +"', '" + ItemDesc + "', '" + Cost + "')";
         }
 
+        /// <summary>
+        /// Updates an existing item in the ItemDesc table
+        /// </summary>
+        /// <param name="ItemCode"></param>
+        /// <param name="ItemDesc"></param>
+        /// <param name="Cost"></param>
+        /// <returns></returns>
         public static string ItemUpdateDescSQL(string ItemCode, string ItemDesc, double Cost)
         {
             return "UPDATE ItemDesc SET ItemCode='" 
@@ -22,16 +36,30 @@ namespace FinalProject.Items
                 + "' WHERE ItemCode='" + ItemCode + "'";        
         }
 
+        /// <summary>
+        /// Deletes an item from the ItemDesc table
+        /// </summary>
+        /// <param name="ItemCode"></param>
+        /// <returns></returns>
         public static string ItemDeleteDescSQL(string ItemCode)
         {
             return "DELETE FROM ItemDesc WHERE ItemCode = '" +  ItemCode +"'";
         }
 
+        /// <summary>
+        /// Selects one item from the ITemDesc table
+        /// </summary>
+        /// <param name="ItemCode"></param>
+        /// <returns></returns>
         public static string ItemSelectDescSQL(string ItemCode)
         {
             return "SELECT * FROM ItemDesc WHERE ItemCode = '" +  ItemCode +"'";
         }
 
+        /// <summary>
+        /// Selects all items in the ItemDesc table.
+        /// </summary>
+        /// <returns></returns>
         public static string ItemsSelectDescSQL()
         {
             return "SELECT * FROM ItemDesc";
