25fd932b0eac5f55823d523e4a2b3687140ee331
Rachel Martinez <rachelmartinez101@gmail.com>
2018-11-19 23:03:28 -0700

    Added SQLSearch statements and data passing logic

--- FinalProject/Search/clsSearchSQL.cs
+++ FinalProject/Search/clsSearchSQL.cs
@@ -18,12 +18,71 @@ namespace FinalProject.Search
 
         #region sqlSearch Public Methods
 
-        //filter by InvoiceNum
+        /// <summary>
+        /// SQL statement to initally load the datagrid with invoice number
+        /// </summary>
+        /// <returns>sql statement</returns>
+        public string getInvoiceNum()
+        {
+            sql = "SELECT InvoiceNum FROM Invoice";
 
-        //filter by InvoiceDate
+            return sql;
+        }
 
-        //filter by InvoiceTotalCost
+        /// <summary>
+        /// SQL statement to initally load the datagrid with invoice date
+        /// </summary>
+        /// <returns>sql statement</returns>
+        public string getInvoiceDate()
+        {
+            sql = "SELECT InvoiceDate FROM Invoice";
 
+            return sql;
+        }
+
+        /// <summary>
+        /// SQL statement to initally load the datagrid with total cost
+        /// </summary>
+        /// <returns>sql statement</returns>
+        public string getInvoiceTotalCost()
+        {
+            sql = "SELECT TotalCost FROM Invoices";
+
+            return sql;
+        }
+
+        /// <summary>
+        /// SQL statement to filter the invoiceNum and invoiceDate by invoiceNum combobox
+        /// </summary>
+        /// <returns>sql statement</returns>
+        public string filterByInvoiceNum(int invoiceNum)
+        {
+            sql = "SELECT InvoiceNum, InvoiceDate, TotalCost FROM Invoice WHERE invoiceNum = " + invoiceNum;
+
+            return sql;
+        }
+
+        /// <summary>
+        /// SQL statement to filter the invoiceNum invoiceDate and TotalCost by invoiceDate combobox
+        /// </summary>
+        /// <returns>sql statement</returns>
+        public string filterByInvoiceDate(int invoiceDate)
+        {
+            sql = "SELECT InvoiceNum, InvoiceDate, TotalCost FROM Invoice WHERE invoiceDate = " + invoiceDate;
+
+            return sql;
+        }
+
+        /// <summary>
+        /// SQL statement to filter the totalCost invoiceDate and TotalCost by TotalCost combobox
+        /// </summary>
+        /// <returns>sql statement</returns>
+        public string filterByInvoiceTotalCost(int totalCost)
+        {
+            sql = "SELECT InvoiceNum, InvoiceDate, TotalCost FROM Invoices WHERE TotalCost = " + totalCost;
+
+            return sql;
+        }
         #endregion
 
     }
